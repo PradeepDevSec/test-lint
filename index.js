c609eee1dec0519ac761d579b1dab196e6be9fef
Pradeep <pradeep@devseccops.ai>
2026-01-29 22:10:29 +0530

    fix linting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const add = require('./app');
 
 if (add(2, 3) !== 5) {
-  throw new Error("Test failed");
+  throw new Error('Test failed');
 }
 
-console.log(" Test passed");
+console.log(' Test passed');
